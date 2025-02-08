357c4cb20ff78ec7359022c638b523ddfac1a0cc
Paul-Sebastian Manole <pmanole@volvocars.com>
2025-02-08 12:40:19 +0200

    disable vtsls formatting conflicting with eslint and prettier

--- lua/community.lua
+++ lua/community.lua
@@ -4,65 +4,35 @@
 
 ---@type LazySpec
 return {
-  -- Add the community repository of plugin specifications FIRST!
   "AstroNvim/astrocommunity",
 
-  -- import/override with your plugins folder:
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- Available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- Color Schemes:
-  -- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/colorscheme
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  -- { import = "astrocommunity.colorscheme.github-nvim-theme" },
 
-  -- { import = "astrocommunity.completion.copilot-lua" },
-
-  -- example of importing an entire language pack:
-  -- these packs can set up things such as Treesitter, Language Servers, additional language specific plugins, and more!
-  -- { import = "astrocommunity.pack.rust" },
-  -- { import = "astrocommunity.pack.python" },
-  --
-  -- Lua Language Pack - https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/lua
   { import = "astrocommunity.pack.lua" },
 
-  -- JSON and JSONC Pack
   { import = "astrocommunity.pack.json" },
-
-  -- Markdown Language Pack - adds markdown TS parsers, marksman LSP, prettierd formatter
   { import = "astrocommunity.pack.markdown" },
-
-  -- OCaml Langauge Pack - https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/ocaml
-  -- { import = "astrocommunity.pack.ocaml" },
-  -- Rust Language Pack - https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/rust
-  -- { import = "astrocommunity.pack.rust" },
-  -- SQL Language Pack - https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/sql
-  -- { import = "astrocommunity.pack.sql" },
-  -- TailwindCSS Language Pack - https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/tailwindcss
-  -- { import = "astrocommunity.pack.tailwindcss" },
-  --
-  -- TypeScript Language Pack
-  { import = "astrocommunity.pack.typescript-all-in-one" },
-
-  -- YAML Language Pack
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.yaml" },
-
-  -- Leap quickly to text on screen
-  { import = "astrocommunity.motion.leap-nvim" },
-  -- Enhanced f, F, t, T, /, ? with tags for quickly jumping to matches
-  { import = "astrocommunity.motion.flash-nvim" },
-
-  -- Snippets integrated natively, uses friendly-snippets for expansion
   { import = "astrocommunity.snippet.nvim-snippets" },
 
-  -- Open in GitHub (file or repo) commands
+  -- { import = "astrocommunity.completion.copilot-lua" },
+  -- { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.ocaml" },
+  -- { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.sql" },
+
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
+
   { import = "astrocommunity.git.openingh-nvim" },
-
-  -- Git Diff View, split panes, easier merge conflict solving, etc.
   { import = "astrocommunity.git.diffview-nvim" },
-
-  -- Git Blame view inline
-  { import = "astrocommunity.git.git-blame-nvim" },
-
-  -- Find and Replace:
   { import = "astrocommunity.search.grug-far-nvim" },
+  -- { import = "astrocommunity.git.git-blame-nvim" }, -- inline git blame
 }
--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,41 +1,23 @@
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
+-- https://github.com/AstroNvim/astroui
 -- Configuration documentation can be found with `:h astroui`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
 
 ---@type LazySpec
 return {
   "AstroNvim/astroui",
+  lazy = false,
+  priority = 10000,
+
   ---@type AstroUIOpts
   opts = {
-    -- -- https://github.com/AstroNvim/astrotheme - astrodark, astrolight, astromars
+    -- AstroTheme:
+    -- https://github.com/AstroNvim/astrotheme
+    -- Supports: astrodark, astrolight, astromars
     -- colorscheme = "astromars",
 
-    colorscheme = "github_dark_dimmed",
+    -- Catppuccin:
+    -- colorscheme = "catppuccin-mocha",
 
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    -- highlights = {
-    --   init = { -- this table overrides highlights in all themes
-    --     -- Normal = { bg = "#000000" },
-    --   },
-    --   astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-    --     -- Normal = { bg = "#000000" },
-    --   },
-    -- },
-
-    -- Icons can be configured throughout the interface
-    -- icons = {
-    --   -- configure the loading of the lsp in the status line
-    --   LSPLoading1 = "⠋",
-    --   LSPLoading2 = "⠙",
-    --   LSPLoading3 = "⠹",
-    --   LSPLoading4 = "⠸",
-    --   LSPLoading5 = "⠼",
-    --   LSPLoading6 = "⠴",
-    --   LSPLoading7 = "⠦",
-    --   LSPLoading8 = "⠧",
-    --   LSPLoading9 = "⠇",
-    --   LSPLoading10 = "⠏",
-    -- },
+    colorscheme = "kanagawa-dragon",
   },
 }
